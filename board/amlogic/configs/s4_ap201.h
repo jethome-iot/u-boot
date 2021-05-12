706f2b66a170bb0cb0e6ef0c8b94c611964d7265
Gongwei Chen <gongwei.chen@amlogic.com>
2021-05-12 17:42:38 +0800

    modfify androidboot.hardwar [1/1]
    PD#SWPL-49694
    
    Change-Id: I1dc819c0f84d55fa7c595cb8d9bb5e30b0f9b7fd
    Signed-off-by: Gongwei Chen <gongwei.chen@amlogic.com>

--- board/amlogic/configs/s4_ap201.h
+++ board/amlogic/configs/s4_ap201.h
@@ -131,7 +131,7 @@
                 "frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} cvbsmode=${cvbsmode} "\
                 "osd_reverse=${osd_reverse} video_reverse=${video_reverse} irq_check_en=${Irq_check_en}  "\
                 "androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
-            "setenv bootargs ${bootargs} androidboot.bootloader=${bootloader_version} androidboot.hardware=amlogic;"\
+            "setenv bootargs ${bootargs} androidboot.bootloader=${bootloader_version} androidboot.hardware=ap201;"\
             "run cmdline_keys;"\
             "\0"\
         "switch_bootmode="\
