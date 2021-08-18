52b29af6463f8accac22c35ea793bdfdf97a2aed
Luan Yuan <luan.yuan@amlogic.com>
2021-08-18 12:25:34 +0800

    Uboot: provide disable uboot log info. [1/1]
    PD#SWPL-34040
    
    Problem:
    optimize bootup time need disbale uboot log.
    
    Solution:
    add the interface to disable uboot log.
    //#define CONFIG_SILENT_CONSOLE
    default enable uboot log.
    Define this macro if the project needs disable uboot info log.
    in board/amlogic/config/xxx.h.
    
    Verify:
    verify by oppencas.
    
    Change-Id: I9a7ca4022d2a3c6b477fd8ac4fa664130d1570db
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- board/amlogic/configs/s4_ap232.h
+++ board/amlogic/configs/s4_ap232.h
@@ -101,7 +101,7 @@
         "sdcburncfg=aml_sdc_burn.ini\0"\
         "EnableSelinux=enforcing\0" \
         "recovery_part=recovery\0"\
-        "loglevel=8\0" \
+        "loglevel=1\0" \
         "lock=10101000\0"\
         "recovery_offset=0\0"\
         "cvbs_drv=0\0"\
@@ -118,7 +118,7 @@
         "fatload_dev=usb\0"\
         "fs_type=""rootfstype=ramfs""\0"\
         "initargs="\
-            "init=/init " CONFIG_KNL_LOG_LEVEL "console=ttyS0,921600 no_console_suspend earlycon=aml-uart,0xfe07a000 "\
+            "init=/init console=ttyS0,921600 no_console_suspend earlycon=aml-uart,0xfe07a000 "\
             "ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 loop.max_part=4 "\
             "\0"\
         "upgrade_check="\
@@ -131,7 +131,7 @@
             "\0"\
         "storeargs="\
             "get_bootloaderversion;" \
-            "setenv bootargs ${initargs} ${fs_type} otg_device=${otg_device} "\
+            "setenv bootargs ${initargs} loglevel=${loglevel} ${fs_type} otg_device=${otg_device} "\
                 "logo=${display_layer},loaded,${fb_addr} vout=${outputmode},enable panel_type=${panel_type} "\
                 "hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} "\
                 "hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} " \
