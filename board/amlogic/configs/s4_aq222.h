68c876f363584c73f7a099bfcc17538db623f096
jiayi.zhou <jiayi.zhou@amlogic.com>
2021-08-18 21:35:22 +0800

    gpio_key: add uboot detect key. [1/1]
    PD#SWPL-57596
    
    Problem:
    1.gpiod_2 default low level
    
    Solution:
    1. Set gpiod_2 high level
    
    Verify:
    s4_ap222
    
    Change-Id: If26f9f58aef71a2216a1b214bdcd11181043de8d
    Signed-off-by: jiayi.zhou <jiayi.zhou@amlogic.com>

--- board/amlogic/configs/s4_aq222.h
+++ board/amlogic/configs/s4_aq222.h
@@ -296,7 +296,7 @@
 	    "factory_provision init;"\
             "\0"\
         "upgrade_key="\
-            "if gpio input GPIOD_3; then "\
+	    "if gpio input GPIOD_2; then "\
             "echo detect upgrade key; run update;"\
             "fi;"\
             "\0"\
@@ -307,6 +307,7 @@
             "run upgrade_check;"\
             "run init_display;"\
             "run storeargs;"\
+	    "run upgrade_key;" \
             "bcb uboot-command;"\
             "run switch_bootmode;"
 #else
