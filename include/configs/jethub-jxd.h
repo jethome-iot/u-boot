a58fffb9daaeb5e6236d70d102d245061300cb06
Viacheslav Bocharov <adeep@lexina.in>
2023-12-20 16:12:14 +0300

    fix board config

---
+++ include/configs/jethub-jxd.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for JetHome devices
+ * Copyright (c) 2023 JetHome, Viacheslav Bocharov
+ * Author: Viacheslav Bocharov <adeep@lexina.in>
+ */
+
+#ifndef __JETHUB_CONFIG_H
+#define __JETHUB_CONFIG_H
+
+
+#define BOOTENV_DEV_RESCUE(devtypeu, devtypel, instance) \
+	"bootcmd_rescue=" \
+		"gpio read userbutton PE5; " \
+		"if test ${userbutton} -eq 1;  then " \
+			"setenv bootargs \"console=ttyS3,115200 ubi.mtd=ubifs root=ubi0:rootfs rw rootfstype=ubifs rootwait\";" \
+			"mtd read dtb ${fdt_addr_r}; " \
+			"fdt addr ${fdt_addr_r}; " \
+			"fdt resize 65536; " \
+			"mtd read kernela ${loadaddr}; " \
+			"bootz ${loadaddr} - ${fdt_addr_r};" \
+		"fi;\0"
+
+#define BOOTENV_DEV_NAME_RESCUE(devtypeu, devtypel, instance) \
+	"rescue "
+
+#ifndef BOOT_TARGET_DEVICES
+#define BOOT_TARGET_DEVICES(func) \
+	func(RESCUE, rescue, na) \
+	func(FEL, fel, na) \
+	BOOT_TARGET_DEVICES_MMC(func) \
+	BOOT_TARGET_DEVICES_SCSI(func) \
+	BOOT_TARGET_DEVICES_USB(func) \
+	BOOT_TARGET_DEVICES_PXE(func) \
+	BOOT_TARGET_DEVICES_DHCP(func)
+#endif
+
+#include <configs/sunxi-common.h>
+
+#endif /* __JETHUB_CONFIG_H */
--- include/configs/sunxi-common.h
+++ include/configs/sunxi-common.h
@@ -250,6 +250,7 @@
 #define BOOTENV_DEV_NAME_FEL(devtypeu, devtypel, instance) \
 	"fel "
 
+#ifndef BOOT_TARGET_DEVICES
 #define BOOT_TARGET_DEVICES(func) \
 	func(FEL, fel, na) \
 	BOOT_TARGET_DEVICES_MMC(func) \
@@ -257,6 +258,7 @@
 	BOOT_TARGET_DEVICES_USB(func) \
 	BOOT_TARGET_DEVICES_PXE(func) \
 	BOOT_TARGET_DEVICES_DHCP(func)
+#endif
 
 #ifdef CONFIG_OLD_SUNXI_KERNEL_COMPAT
 #define BOOTCMD_SUNXI_COMPAT \
